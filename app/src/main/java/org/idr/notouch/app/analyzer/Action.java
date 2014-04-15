c22ee3fe6d835c98631dbe61cd2c7f1a37236f09
ms <royan367@gmail.com>
2014-04-15 11:40:00 +0300

    - added a callback functionality for running the action commands

--- app/src/main/java/org/idr/notouch/app/analyzer/Action.java
+++ app/src/main/java/org/idr/notouch/app/analyzer/Action.java
@@ -6,12 +6,14 @@ package org.idr.notouch.app.analyzer;
 public class Action {
 
     private int mNameId;
+    private ActionCallback mActionCallback;
 
     public Action() {
     }
 
-    public Action(int nameId) {
+    public Action(int nameId, ActionCallback actionCallback) {
         mNameId = nameId;
+        mActionCallback = actionCallback;
     }
 
     public int getName() {
@@ -20,5 +22,22 @@ public class Action {
 
     public void setName(int nameId) {
         mNameId = nameId;
+    }
+
+    public ActionCallback getActionCallback() {
+        return mActionCallback;
+    }
+
+    public void setActionCallback(ActionCallback actionCallback) {
+        mActionCallback = actionCallback;
+    }
+
+    public void run() {
+        mActionCallback.onAction();
+    }
+
+
+    static interface ActionCallback {
+        void onAction();
     }
 }
