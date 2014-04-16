a830ab3e340e6e80814474e0a2301c618aaa24f5
ms <royan367@gmail.com>
2014-04-16 23:33:22 +0300

    added concrete SpeechContext and SpeechContextManager classes, but not implemented

---
+++ app/src/main/java/org/idr/notouch/app/analyzer/SpeechContext.java
@@ -0,0 +1,37 @@
+package org.idr.notouch.app.analyzer;
+
+import java.util.List;
+
+
+// TODO boşlukları doldur
+
+
+/**
+ * Created by ms on 16.04.2014.
+ */
+public class SpeechContext implements SpeechContextImpl {
+    @Override
+    public List<Action> getActions() {
+        return null;
+    }
+
+    @Override
+    public void setActions(List<Action> actions) {
+
+    }
+
+    @Override
+    public void addAction(Action action) {
+
+    }
+
+    @Override
+    public void removeAction(Action action) {
+
+    }
+
+    @Override
+    public Action findActionByName(int nameId) {
+        return null;
+    }
+}
---
+++ app/src/main/java/org/idr/notouch/app/analyzer/SpeechContextManager.java
@@ -0,0 +1,35 @@
+package org.idr.notouch.app.analyzer;
+
+
+// TODO metodları doldur
+
+
+/**
+ * Created by ms on 16.04.2014.
+ */
+public class SpeechContextManager implements SpeechContextManagerImpl {
+    @Override
+    public SpeechContextImpl getGlobalSpeechContext() {
+        return null;
+    }
+
+    @Override
+    public SpeechContextImpl getMainContext() {
+        return null;
+    }
+
+    @Override
+    public SpeechContextImpl getCurrentContext() {
+        return null;
+    }
+
+    @Override
+    public SpeechContextImpl getPrevContext() {
+        return null;
+    }
+
+    @Override
+    public void changeLocalContext(SpeechContextImpl speechContext) {
+
+    }
+}
