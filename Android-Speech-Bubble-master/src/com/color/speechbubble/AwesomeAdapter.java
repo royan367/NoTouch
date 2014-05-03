9dcd423e2a28f65e407777a5b8ee7a02b3b18df6
İsmail Arılık <royan367@gmail.com>
2014-05-03 11:05:28 +0300

    speech bubbles reverted to green-orange color with more good appearance

--- Android-Speech-Bubble-master/src/com/color/speechbubble/AwesomeAdapter.java
+++ Android-Speech-Bubble-master/src/com/color/speechbubble/AwesomeAdapter.java
@@ -3,7 +3,6 @@ package com.color.speechbubble;
 import java.util.ArrayList;
 
 import android.content.Context;
-import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
